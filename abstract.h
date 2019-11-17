937f01cb0e5537e23661b4d0469ea235b8f92f33
Pawel Pery <ewienik@post.pl>
2019-11-17 01:35:58 +0100

    WIP

--- abstract.h
+++ abstract.h
@@ -5,12 +5,13 @@
 
 #include <utility>
 
+#include <iostream>
+
 /**/
 
 template <typename T>
 struct Abstract {
     Abstract(T &t) : ref(t) {}
-    Abstract(T &&t) : ref(t) {}
     T &ref;
 };
 
@@ -24,10 +25,15 @@ struct IA {
 template <typename T>
 struct A : virtual IA, virtual Abstract<T> {
     A(T &t) : Abstract<T>(t) {}
-    A(T &&t) : Abstract<T>(std::move(t)) {}
 
-    auto a1() -> int override { return Abstract<T>::ref.a1(); }
-    auto a2() -> int override { return Abstract<T>::ref.a2(); }
+    auto a1() -> int override {
+        std::cout << "a1: " << this << ", " << &(Abstract<T>::ref) << "\n" << std::flush;
+        return Abstract<T>::ref.a1();
+    }
+    auto a2() -> int override {
+        std::cout << "a2: " << this << ", " << &(Abstract<T>::ref) << "\n" << std::flush;
+        return Abstract<T>::ref.a2();
+    }
 };
 
 /**/
@@ -40,10 +46,15 @@ struct IB {
 template <typename T>
 struct B : virtual IB, virtual Abstract<T> {
     B(T &t) : Abstract<T>(t) {}
-    B(T &&t) : Abstract<T>(std::move(t)) {}
 
-    auto b1() -> int override { return Abstract<T>::ref.b1(); }
-    auto b2() -> int override { return Abstract<T>::ref.b2(); }
+    auto b1() -> int override {
+        std::cout << "b1: " << this << ", " << &(Abstract<T>::ref) << "\n" << std::flush;
+        return Abstract<T>::ref.b1();
+    }
+    auto b2() -> int override {
+        std::cout << "b2: " << this << ", " << &(Abstract<T>::ref) << "\n" << std::flush;
+        return Abstract<T>::ref.b2();
+    }
 };
 
 /**/
@@ -51,9 +62,14 @@ struct B : virtual IB, virtual Abstract<T> {
 struct IC : virtual IA, virtual IB {};
 
 template <typename T>
-struct C : virtual IC, virtual A<T>, virtual B<T>, virtual Abstract<T> {
-    C(T &t) : A<T>(t), B<T>(t), Abstract<T>(t) {}
-    C(T &&t) : A<T>(t), B<T>(t), Abstract<T>(t) {}
+struct C : virtual IC, virtual A<T>, virtual B<T> {
+    C(T &t) : A<T>(t), B<T>(t), Abstract<T>(t) { std::cout << "C: " << this << ", " << &t << "\n" << std::flush; }
+
+    operator IC *() const {
+        auto ptr = reinterpret_cast<IC *>(&(Abstract<T>::ref));
+        std::cout << this << ", " << ptr << "\n" << std::flush;
+        return ptr;
+    }
 };
 
 /**/
--- tests.cpp
+++ tests.cpp
@@ -4,8 +4,6 @@
 
 /**/
 
-/**/
-
 namespace {
 
 /**/
@@ -64,20 +62,27 @@ struct CImpl2 : public AImpl, public BImpl {};
 
 /**/
 
+struct Use {
+    Use(IC *ic) : ic(ic) { std::cout << "Use: " << this << ", " << ic << "\n" << std::flush; }
+    IC *ic; // NOLINT
+};
+
+/**/
+
 } // namespace
 
 /**/
 
+#if 0
 TEST_CASE("IA") {
     auto aimpl = AImpl{};
     auto a = A(aimpl);
-    IA &ia = a;
 
     REQUIRE(2 == [](IA &&ia) { return ia.a1(); }(A(aimpl)));
     REQUIRE(3 == [](IA &&ia) { return ia.a2(); }(A(aimpl)));
 
-    REQUIRE(2 == [](IA &ia) { return ia.a1(); }(ia));
-    REQUIRE(3 == [](IA &ia) { return ia.a2(); }(ia));
+    REQUIRE(2 == [](IA &ia) { return ia.a1(); }(a));
+    REQUIRE(3 == [](IA &ia) { return ia.a2(); }(a));
 
     REQUIRE(2 == [](IA *ia) { return ia->a1(); }(&a));
     REQUIRE(3 == [](IA *ia) { return ia->a2(); }(&a));
@@ -90,13 +95,12 @@ TEST_CASE("IA") {
 TEST_CASE("IB") {
     auto bimpl = BImpl{};
     auto b = B(bimpl);
-    IB &ib = b;
 
     REQUIRE(4 == [](IB &&ib) { return ib.b1(); }(B(bimpl)));
     REQUIRE(5 == [](IB &&ib) { return ib.b2(); }(B(bimpl)));
 
-    REQUIRE(4 == [](IB &ib) { return ib.b1(); }(ib));
-    REQUIRE(5 == [](IB &ib) { return ib.b2(); }(ib));
+    REQUIRE(4 == [](IB &ib) { return ib.b1(); }(b));
+    REQUIRE(5 == [](IB &ib) { return ib.b2(); }(b));
 
     REQUIRE(4 == [](IB *ib) { return ib->b1(); }(&b));
     REQUIRE(5 == [](IB *ib) { return ib->b2(); }(&b));
@@ -109,17 +113,16 @@ TEST_CASE("IB") {
 TEST_CASE("IC1") {
     auto cimpl = CImpl1{};
     auto c = C(cimpl);
-    IC &ic = c;
 
     REQUIRE(6 == [](IC &&ic) { return ic.a1(); }(C(cimpl)));
     REQUIRE(7 == [](IC &&ic) { return ic.a2(); }(C(cimpl)));
     REQUIRE(8 == [](IC &&ic) { return ic.b1(); }(C(cimpl)));
     REQUIRE(9 == [](IC &&ic) { return ic.b2(); }(C(cimpl)));
 
-    REQUIRE(6 == [](IC &ic) { return ic.a1(); }(ic));
-    REQUIRE(7 == [](IC &ic) { return ic.a2(); }(ic));
-    REQUIRE(8 == [](IC &ic) { return ic.b1(); }(ic));
-    REQUIRE(9 == [](IC &ic) { return ic.b2(); }(ic));
+    REQUIRE(6 == [](IC &ic) { return ic.a1(); }(c));
+    REQUIRE(7 == [](IC &ic) { return ic.a2(); }(c));
+    REQUIRE(8 == [](IC &ic) { return ic.b1(); }(c));
+    REQUIRE(9 == [](IC &ic) { return ic.b2(); }(c));
 
     REQUIRE(6 == [](IC *ic) { return ic->a1(); }(&c));
     REQUIRE(7 == [](IC *ic) { return ic->a2(); }(&c));
@@ -134,17 +137,16 @@ TEST_CASE("IC1") {
 TEST_CASE("IC2") {
     auto cimpl = CImpl2{};
     auto c = C(cimpl);
-    IC &ic = c;
 
     REQUIRE(2 == [](IC &&ic) { return ic.a1(); }(C(cimpl)));
     REQUIRE(3 == [](IC &&ic) { return ic.a2(); }(C(cimpl)));
     REQUIRE(4 == [](IC &&ic) { return ic.b1(); }(C(cimpl)));
     REQUIRE(5 == [](IC &&ic) { return ic.b2(); }(C(cimpl)));
 
-    REQUIRE(2 == [](IC &ic) { return ic.a1(); }(ic));
-    REQUIRE(3 == [](IC &ic) { return ic.a2(); }(ic));
-    REQUIRE(4 == [](IC &ic) { return ic.b1(); }(ic));
-    REQUIRE(5 == [](IC &ic) { return ic.b2(); }(ic));
+    REQUIRE(2 == [](IC &ic) { return ic.a1(); }(c));
+    REQUIRE(3 == [](IC &ic) { return ic.a2(); }(c));
+    REQUIRE(4 == [](IC &ic) { return ic.b1(); }(c));
+    REQUIRE(5 == [](IC &ic) { return ic.b2(); }(c));
 
     REQUIRE(2 == [](IC *ic) { return ic->a1(); }(&c));
     REQUIRE(3 == [](IC *ic) { return ic->a2(); }(&c));
@@ -160,6 +162,28 @@ TEST_CASE("IC2") {
     BENCHMARK("Pointer") { pointer(&c); };
     BENCHMARK("Move") { move(C(cimpl)); };
     BENCHMARK("A1") { a1(C(cimpl)); };
+}
+
+#endif
+/**/
+
+TEST_CASE("Use") {
+    auto cimpl = CImpl2{};
+    std::cout << "Use cimpl: " << &cimpl << ", " << sizeof(C(cimpl)) << ", " << sizeof(IC) << ", "
+              << sizeof(Abstract<CImpl2>) << "\n"
+              << std::flush;
+    auto ptr = static_cast<IC *>((C(cimpl)));
+    std::cout << "Use ptr: " << ptr << "\n" << std::flush;
+    auto use = Use(C(cimpl));
+    // auto use = [&]() -> Use { return Use(C(cimpl)); }();
+
+    REQUIRE(2 == use.ic->a1());
+    REQUIRE(3 == use.ic->a2());
+    REQUIRE(4 == use.ic->b1());
+    REQUIRE(5 == use.ic->b2());
+
+    REQUIRE(cimpl.AImpl::count == 2);
+    REQUIRE(cimpl.BImpl::count == 2);
 }
 
 /**/
